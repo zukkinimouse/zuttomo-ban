0e36a7e443db10ff9b3c8198f463ff800811be22
zukkinimouse <tirolchoco.taiyaki@gmail.com>
2026-03-05 18:09:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zuttomo-ban-v4';
+const CACHE_NAME = 'zuttomo-ban-v5';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
